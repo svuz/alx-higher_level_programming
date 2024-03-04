81ef12df42ff96148d09d1d2d598bd8cf7ea9926
s <smblackeye@gmail.com>
2024-03-03 23:56:25 -0800

    Fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(r.text))
-    print("\t- content: {}".format(r.text))
+    print("Body response:$")
+    print("\t- type: {}$".format(type(r.text)))
+    print("\t- content: {}$".format(r.text))
