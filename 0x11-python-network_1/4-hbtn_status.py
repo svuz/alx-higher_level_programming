7243222c6f92dbba9be9829be816dc9d180b2700
s <smblackeye@gmail.com>
2024-03-03 23:59:50 -0800

    Fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status."""
+"""fetching url"""
 import requests
 
 
-if __name__ == "__main__":
-    r = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response:$")
-    print("\t- type: ",type(r.text))
-    print("\t- content: ",r.text)
+r = requests.get('https://alx-intranet.hbtn.io/status')
+print("Body response:")
+print(f"\t- type: {type(r.text)}")
+print(f"\t- content: {r.text}")
